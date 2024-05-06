911e86d3823785cc3f2c4588a60a80b3faee1a08
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-05-06 10:12:04 +0600

    Initial Commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Rezaul Islam on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
